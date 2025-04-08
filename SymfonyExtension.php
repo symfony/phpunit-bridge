12f470d3da5c794d477573dea9afac6ebed9fd63
Christian Flothmann <christian.flothmann@open.de>
2025-04-08 12:47:50 +0200

    properly clean up mocked features after tests have run
    If a test has been skipped or if it errored, the mocked PHP functions
    must be cleaned up as well.

--- SymfonyExtension.php
+++ SymfonyExtension.php
@@ -11,12 +11,18 @@
 
 namespace Symfony\Bridge\PhpUnit;
 
+use PHPUnit\Event\Test\BeforeTestMethodErrored;
+use PHPUnit\Event\Test\BeforeTestMethodErroredSubscriber;
+use PHPUnit\Event\Test\Errored;
+use PHPUnit\Event\Test\ErroredSubscriber;
+use PHPUnit\Event\Test\Finished;
+use PHPUnit\Event\Test\FinishedSubscriber;
+use PHPUnit\Event\Test\Skipped;
+use PHPUnit\Event\Test\SkippedSubscriber;
 use PHPUnit\Runner\Extension\Extension;
 use PHPUnit\Runner\Extension\Facade;
 use PHPUnit\Runner\Extension\ParameterCollection;
 use PHPUnit\TextUI\Configuration\Configuration;
-use Symfony\Bridge\PhpUnit\Extension\DisableClockMockSubscriber;
-use Symfony\Bridge\PhpUnit\Extension\DisableDnsMockSubscriber;
 use Symfony\Bridge\PhpUnit\Extension\EnableClockMockSubscriber;
 use Symfony\Bridge\PhpUnit\Extension\RegisterClockMockSubscriber;
 use Symfony\Bridge\PhpUnit\Extension\RegisterDnsMockSubscriber;
@@ -38,7 +44,37 @@ class SymfonyExtension implements Extension
 
         $facade->registerSubscriber(new RegisterClockMockSubscriber());
         $facade->registerSubscriber(new EnableClockMockSubscriber());
-        $facade->registerSubscriber(new DisableClockMockSubscriber());
+        $facade->registerSubscriber(new class implements ErroredSubscriber {
+            public function notify(Errored $event): void
+            {
+                SymfonyExtension::disableClockMock();
+                SymfonyExtension::disableDnsMock();
+            }
+        });
+        $facade->registerSubscriber(new class implements FinishedSubscriber {
+            public function notify(Finished $event): void
+            {
+                SymfonyExtension::disableClockMock();
+                SymfonyExtension::disableDnsMock();
+            }
+        });
+        $facade->registerSubscriber(new class implements SkippedSubscriber {
+            public function notify(Skipped $event): void
+            {
+                SymfonyExtension::disableClockMock();
+                SymfonyExtension::disableDnsMock();
+            }
+        });
+
+        if (interface_exists(BeforeTestMethodErroredSubscriber::class)) {
+            $facade->registerSubscriber(new class implements BeforeTestMethodErroredSubscriber {
+                public function notify(BeforeTestMethodErrored $event): void
+                {
+                    SymfonyExtension::disableClockMock();
+                    SymfonyExtension::disableDnsMock();
+                }
+            });
+        }
 
         if ($parameters->has('dns-mock-namespaces')) {
             foreach (explode(',', $parameters->get('dns-mock-namespaces')) as $namespace) {
@@ -47,6 +83,21 @@ class SymfonyExtension implements Extension
         }
 
         $facade->registerSubscriber(new RegisterDnsMockSubscriber());
-        $facade->registerSubscriber(new DisableDnsMockSubscriber());
+    }
+
+    /**
+     * @internal
+     */
+    public static function disableClockMock(): void
+    {
+        ClockMock::withClockMock(false);
+    }
+
+    /**
+     * @internal
+     */
+    public static function disableDnsMock(): void
+    {
+        DnsMock::withMockedHosts([]);
     }
 }
