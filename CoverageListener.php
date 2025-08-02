ddce87db4f5c12fcff191f294f3b70125b333038
W0rma <beck.worma@gmail.com>
2025-08-02 13:32:54 +0200

    Reflection*::setAccessible() has no effect as of PHP 8.1

--- CoverageListener.php
+++ CoverageListener.php
@@ -86,7 +86,9 @@ class CoverageListener implements TestListener
     private function addCoversForClassToAnnotationCache(Test $test, array $covers): void
     {
         $r = new \ReflectionProperty(TestUtil::class, 'annotationCache');
-        $r->setAccessible(true);
+        if (\PHP_VERSION_ID < 80100) {
+            $r->setAccessible(true);
+        }
 
         $cache = $r->getValue();
         $cache = array_replace_recursive($cache, [
@@ -103,7 +105,9 @@ class CoverageListener implements TestListener
         $docBlock = Registry::getInstance()->forClassName(\get_class($test));
 
         $symbolAnnotations = new \ReflectionProperty($docBlock, 'symbolAnnotations');
-        $symbolAnnotations->setAccessible(true);
+        if (\PHP_VERSION_ID < 80100) {
+            $symbolAnnotations->setAccessible(true);
+        }
 
         // Exclude internal classes; PHPUnit 9.1+ is picky about tests covering, say, a \RuntimeException
         $covers = array_filter($covers, function (string $class) {
